04403ab136d2d9db24c0c1cebd09be7a8e349017
Brian <brian.timakela@hotmail.com>
2026-05-16 01:15:43 +0100

    Asked User to enter first number

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Calculator
 {
@@ -10,7 +11,14 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            int num1;
+
             Console.WriteLine("Hello, welcome to the calculator program!");
+
+            Console.WriteLine("Please enter the first number");
+
+            num1= Convert.ToInt32(Console.ReadLine());
+            
 
             Console.ReadKey();
         }
