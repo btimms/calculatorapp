f65152f4db20612e275f8febb2e7bc6452472c4d
Brian <brian.timakela@hotmail.com>
2026-05-16 23:14:16 +0100

    Part 2: Changed the numbers from integers to floats to make the answers take decimal numbers into account

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,20 +11,20 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            int num1;
-            int num2;
+            float num1;
+            float num2;
 
-            int result;
+            float result;
 
             string answer;
 
             Console.WriteLine("Hello, welcome to the calculator program!");
             
             Console.WriteLine("Please enter the first number");
-            num1= Convert.ToInt32(Console.ReadLine());
+            num1= float.Parse(Console.ReadLine());
 
             Console.WriteLine("Please enter the second number");
-            num2 = Convert.ToInt32(Console.ReadLine());
+            num2 = float.Parse(Console.ReadLine());
 
             Console.WriteLine("What type of operation would you like to do?");
             Console.WriteLine("Please enter A for addition, S for subtraction, M for multiplication or any other key for division");
