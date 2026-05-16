434b30ece5f9ab03c01fb1e53abdcba0c9185fc4
Brian <brian.timakela@hotmail.com>
2026-05-16 01:23:50 +0100

    Asked User to enter second number

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -12,13 +12,16 @@ namespace Calculator
         static void Main(string[] args)
         {
             int num1;
+            int num2;
 
             Console.WriteLine("Hello, welcome to the calculator program!");
-
-            Console.WriteLine("Please enter the first number");
-
-            num1= Convert.ToInt32(Console.ReadLine());
             
+            Console.WriteLine("Please enter the first number");
+            num1= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
 
             Console.ReadKey();
         }
