71193892c45c28be0bb0fe474c2d1b6e8f78db45
Brian <brian.timakela@hotmail.com>
2026-05-16 02:11:39 +0100

    Makes a decision based on the user has entered

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -14,6 +14,8 @@ namespace Calculator
             int num1;
             int num2;
 
+            int result;
+
             string answer;
 
             Console.WriteLine("Hello, welcome to the calculator program!");
@@ -25,10 +27,29 @@ namespace Calculator
             num2 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("What type of operation would you like to do?");
-            Console.WriteLine("Please enter A for addition, S for subtraction, M for multiplication or D for division");
+            Console.WriteLine("Please enter A for addition, S for subtraction, M for multiplication or any other key for division");
 
             answer = Console.ReadLine();
 
+            if (answer == "A")
+            {
+                result = num1 + num2;
+            }
+
+            else if (answer == "S")
+            {
+                result = num1 - num2;
+            }
+
+            else if (answer == "M") 
+            {
+                result = num1 * num2;
+            }
+
+            else
+            {
+                result = num1 / num2;
+            }
 
             Console.ReadKey();
         }
